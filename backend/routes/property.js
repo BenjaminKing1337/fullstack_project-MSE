a3d0ca00b872d6557c3cd4e343ea515fb7ca41e6
Anna Lopez <annalopezribo@gmail.com>
2022-11-06 14:55:30 +0100

    New property query

--- backend/routes/property.js
+++ backend/routes/property.js
@@ -1,17 +1,15 @@
-var config = require("../dbconfig");
-const sql = require("mssql");
+var config = require('../dbconfig');
+const sql = require('mssql');
 
-const Express = require("express");
+const Express = require('express');
 const router = Express.Router();
-const Property = require("../classes/Property");
+const Property = require('../classes/Property');
 
 //GetALL
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     let pool = await sql.connect(config);
-    const Properties = await pool
-    .request()
-    .execute("spGetAllProperties");
+    const Properties = await pool.request().execute('spGetAllProperties');
     res.json(Properties.recordsets[0]);
     sql.close();
   } catch (error) {
@@ -20,13 +18,13 @@ router.get("/", async (req, res) => {
   }
 });
 //GetById
-router.get("/get/:id", async (req, res) => {
+router.get('/get/:id', async (req, res) => {
   try {
     let pool = await sql.connect(config);
     const PropertyById = await pool
       .request()
-      .input("id", sql.Int, parseInt(req.params.id))
-      .execute("spGetProperty");
+      .input('id', sql.Int, parseInt(req.params.id))
+      .execute('spGetProperty');
     res.json(PropertyById.recordset[0]);
     sql.close();
   } catch (error) {
@@ -35,25 +33,31 @@ router.get("/get/:id", async (req, res) => {
   }
 });
 //CreateNew
-router.post("/new", async (req, res) => {
+router.post('/new', async (req, res) => {
   try {
-    const NewProperty = new Property(req.body.id, req.body.number, req.body.address, req.body.name);
+    const NewProperty = new Property(
+      undefined, // best way of handling this?
+      req.body.number,
+      req.body.address,
+      req.body.name
+    );
     console.log(NewProperty);
     let pool = await sql.connect(config);
     const SavedProperty = await pool
-    .request()
-    // .input("id", sql.Int, Property.id)
-    .input("number", sql.NVarChar, Property.number)
-    .input("address", sql.NVarChar, Property.address)
-    .input("name", sql.NVarChar, Property.name)
-    .query("insert into tblProperty values (number, address, name)");
-    res.json(SavedProperty.recordsets);
+      .request()
+      .input('number', sql.NVarChar, NewProperty.number)
+      .input('address', sql.NVarChar, NewProperty.address)
+      .input('name', sql.NVarChar, NewProperty.name)
+      .query(
+        'insert into tblProperty(number, address, name) values (@number,@address,@name)'
+      );
+    // let a = res.json(SavedProperty.recordsets);
     sql.close();
   } catch (error) {
     res.status(400).json({ error });
-    
+
     sql.close();
   }
-})
+});
 
 module.exports = router;
