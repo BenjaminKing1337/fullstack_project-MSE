7c7af4abac4f8e6485418080ff994eb4ec31e495
BenjaminKing1337 <benjaminking1337@live.com>
2022-12-26 02:00:07 +0100

    Update baseURL.js

--- frontend/src/modules/baseURL.js
+++ frontend/src/modules/baseURL.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://localhost:3000/api';
-// const baseURL = 'https://mse-a9ir.onrender.com/api';
+// const baseURL = 'http://localhost:3000/api';
+const baseURL = 'https://mse-a9ir.onrender.com/api';
 
 export default baseURL;
